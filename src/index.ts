9e997a88f1d800cf9d4ee1e5ecb337d186c6b60f
florianjuesten <flojuesten@gmail.com>
2021-12-28 14:18:21 +0100

    added winston

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
 import { IApp } from './app'
 import { container } from './container'
 import { TYPES } from './types'
+import { logger } from './logger'
+
+process.on('uncaughtException', function (err) {
+  if (err) {
+    logger.error(`caughtException but no error msg${err.stack}`)
+    process.exit(1)
+  }
+})
 
 function bootstrap() {
   try {
     const app = container.get<IApp>(TYPES.IApp)
     app.main()
   } catch (error) {
-    console.error(error)
+    logger.error(error)
   }
 }
 
